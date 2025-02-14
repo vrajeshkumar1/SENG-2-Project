c5634286fe40e1e95a361ae6d4aa87e5cf17e34a
Esteban Flores <160200502+EstebanF1612@users.noreply.github.com>
2025-02-14 01:57:26 +0000

    numCitiesFix & aesthetic changes

--- main.cpp
+++ main.cpp
@@ -5,97 +5,14 @@
 #include <iomanip>
 #include "City.h"
 using namespace std;
-int rows=0;
+
+int rows = 0;
 City *cities = new City[100];
+
 int menu();
-void getRowInfo(istream &file, int rowAnalyzed, string (&infoHolder)[7])
-{
-    string line;
-
-    file.clear();
-    file.seekg(0, ios::beg); // move read pointer to the beginning of the file
-
-    for (int i = 1; i <= rowAnalyzed; i++)
-    {
-        getline(file, line); // find row to analyze
-    }
-
-    size_t pos = 0;
-    int cellIndex = 0;
-
-    while ((pos = line.find(',')) != string::npos && cellIndex < 7)
-    {
-        infoHolder[cellIndex++] = line.substr(0, pos);
-        line.erase(0, pos + 1);
-    }
-    if (cellIndex < 7)
-    { // if there are less than 6 cells in the row
-        infoHolder[cellIndex] = line;
-    }
-}
-
-void printCities(){
-    cout << setw(20) << "City Name" << setw(20) << "Population" << setw(25) << "Avg Dist Enemy Base" << setw(25) << "Avg Dist Friendly Base" << setw(30) << "Est Enemy Infantry Power" << setw(30) << "Est Friendly Infantry Power" << setw(23) << "Political Interference" << setw(15) << "Risk Factor" << endl;
-    for (int i = 0; i < rows - 1; i++)
-    {
-        cout << setw(20) << cities[i].getName() << setw(20) << cities[i].getPopulation() << setw(25) << cities[i].getAvgDistEnemyBase() << setw(25) << cities[i].getAvgDistFriendlyBase() << setw(30) << cities[i].getEstEnemyInfantryPower() << setw(30) << cities[i].getEstFriendlyInfantryPower() << setw(23) << cities[i].getPoliticalInterference() << setw(15) << cities[i].getRiskFactor() << endl;
-    }
-}
-
-void adminDashboard(){
-    int option;
-    do{
-        cout<<"============ Admin Dashboard ============\n1. Add City\n2. Modify City Information\n3. Delete City\n0. Return to Main Menu\n\nOption: ";
-        cin>> option;
-    } while (option < 0 || option > 3);
-
-    switch(option){
-        case 0:
-            menu();
-            break;
-        case 1:
-            //As risk factor is scaled relatively, when cities are added or modified, it will be recalculated for all
-            for(int i=0; i<cities[0].numCities; i++){
-              cities[i].defineRiskFactor();
-            }
-            break;
-        case 2:
-            //As risk factor is scaled relatively, when cities are added or modified, it will be recalculated for all
-                for(int i=0; i<cities[0].numCities; i++){
-                    cities[i].defineRiskFactor();
-                }
-            break;
-        case 3:
-
-            break;
-    }
-}
-
-int menu(){
-    int option;
-    do{
-        cout<<"============ Menu ============\n1. Display Current Cities\n2. Most Risky City\n3. Admin Dashboard Login\n0. Exit\n\nOption: ";
-        cin>> option;
-    } while (option < 0 || option > 3);
-
-    switch(option){
-        case 0:
-            return 0;
-            break;
-        case 1:
-            printCities();
-            break;
-        case 2:
-            
-            break;
-        case 3:
-            adminDashboard();
-            break;
-    }
-
-    return 1;
-    
-}
+void getRowInfo(istream &file, int rowAnalyzed, string (&infoHolder)[7]);
+void printCities();
+void adminDashboard();
 
 int main()
 {
@@ -125,11 +42,109 @@ int main()
 
     int finalChoice = 1;
 
-    while (finalChoice == 1){
+    while (finalChoice == 1)
+    {
         finalChoice = menu();
     }
 
-    
-
     return 0;
+}
+
+int menu()
+{
+    int option;
+    do
+    {
+        cout << "============ Menu ============\n1. Display Current Cities\n2. Most Risky City\n3. Admin Dashboard Login\n0. Exit\n\nOption: ";
+        cin >> option;
+    } while (option < 0 || option > 3);
+
+    switch (option)
+    {
+    case 0:
+        return 0;
+        break;
+    case 1:
+        printCities();
+        break;
+    case 2:
+
+        break;
+    case 3:
+        adminDashboard();
+        break;
+    }
+
+    return 1;
+}
+
+void getRowInfo(istream &file, int rowAnalyzed, string (&infoHolder)[7])
+{
+    string line;
+
+    file.clear();
+    file.seekg(0, ios::beg); // move read pointer to the beginning of the file
+
+    for (int i = 1; i <= rowAnalyzed; i++)
+    {
+        getline(file, line); // find row to analyze
+    }
+
+    size_t pos = 0;
+    int cellIndex = 0;
+
+    while ((pos = line.find(',')) != string::npos && cellIndex < 7)
+    {
+        infoHolder[cellIndex++] = line.substr(0, pos);
+        line.erase(0, pos + 1);
+    }
+    if (cellIndex < 7)
+    { // if there are less than 6 cells in the row
+        infoHolder[cellIndex] = line;
+    }
+}
+
+void printCities()
+{
+    cout<<"============================================================ Cities: "<< City::getNumCities()<<" ===================================================================================\n";
+    cout << setw(20) << "City Name|" << setw(20) << "Population|" << setw(25) << "Avg Dist Enemy Base|" << setw(25) << "Avg Dist Friendly Base|" << setw(29) << "Est Enemy Infantry Power|" << setw(29) << "Est Friendly Infantry Power|" << setw(24) << "Political Interference|" << setw(15) << "Risk Factor|" << endl;
+    cout<<"------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n";
+    for (int i = 0; i < rows - 1; i++)
+    {
+        cout << setw(20) << cities[i].getName() << setw(20) << cities[i].getPopulation() << setw(25) << cities[i].getAvgDistEnemyBase() << setw(25) << cities[i].getAvgDistFriendlyBase() << setw(29) << cities[i].getEstEnemyInfantryPower() << setw(29) << cities[i].getEstFriendlyInfantryPower() << setw(24) << cities[i].getPoliticalInterference() << setw(15) << cities[i].getRiskFactor() << endl;
+    }
+}
+
+void adminDashboard()
+{
+    int option;
+    do
+    {
+        cout << "============ Admin Dashboard ============\n1. Add City\n2. Modify City Information\n3. Delete City\n0. Return to Main Menu\n\nOption: ";
+        cin >> option;
+    } while (option < 0 || option > 3);
+
+    switch (option)
+    {
+    case 0:
+        menu();
+        break;
+    case 1:
+        // As risk factor is scaled relatively, when cities are added or modified, it will be recalculated for all
+        for (int i = 0; i < City::getNumCities(); i++)
+        {
+            cities[i].defineRiskFactor();
+        }
+        break;
+    case 2:
+        // As risk factor is scaled relatively, when cities are added or modified, it will be recalculated for all
+        for (int i = 0; i < City::getNumCities(); i++)
+        {
+            cities[i].defineRiskFactor();
+        }
+        break;
+    case 3:
+
+        break;
+    }
 }
