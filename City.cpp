603682266a99e55fcf2920885feaba7cb0ba939f
vrajeshkumar1 <vrajeshpc@gmail.com>
2025-02-16 22:54:00 -0800

    Risk factor should work now

--- City.cpp
+++ City.cpp
@@ -144,18 +144,30 @@ void City::defineRiskFactor()
      float weights[6]={0.35,-0.65,0.65,0.5,-0.5,0.2};
      float scaledValues[this->getNumCities()][6];
      float temp[this->getNumCities()];
+     float maxTemp;
+     float minTemp;
      for (int i = 0; i < 6; i++){
        for(int j=0; j<this->getNumCities(); j++){
          temp[j]=cities[j]->factors[i];
      }
+         maxTemp=temp[0];
+         minTemp=temp[0];
+         for(int j=0; j<this->getNumCities(); j++){
+           if(temp[j]>maxTemp){
+             maxTemp=temp[j];
+           }
+           if(temp[j]<minTemp){
+             minTemp=temp[j];
+           }
+         }
          if(i!=1&&i!=2){
             for(int j=0; j<this->getNumCities(); j++){
-                scaledValues[j][i]=
+                scaledValues[j][i]=(temp[j]-minTemp)/(maxTemp-minTemp);
             }
         }
         else{
             for(int j=0; j<this->getNumCities(); j++){
-                scaledValues[j][i]=
+                scaledValues[j][i]=(maxTemp-temp[j])/(maxTemp-minTemp);
             }
         }
      }
